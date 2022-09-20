e108ebf656ab3a53f1692e71fcbf3cd6d0b8675b
Cintia Arce <cintiaayelenarce@gmail.com>
2022-09-20 13:08:41 -0300

    developer

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,9 @@ import "./style/Home.css";
 export const Home: React.FC = () => {
   return (
     <main className="page-container home-container">
-      <h1>Frontend Task</h1>
+      <h1>
+        Frontend Task <small>by Cintia Arce</small>
+      </h1>
       <p>
         On the "search" page you can search any user from github. <br />
         While in "history", you will find all your results you get from "search"
